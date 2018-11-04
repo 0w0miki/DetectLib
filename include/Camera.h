c9dff5b5babb954243cc366a7c8b324ea2d239df
miki0w0 <miki0w0@hotmail.com>
2018-11-04 09:32:35 +0800

    init

---
+++ include/Camera.h
@@ -0,0 +1,148 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "GxIAPI.h"
+#include "DxImageProc.h"
+#include "CTimeCounter.h"
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string>
+#include <queue>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include "CameraUtils.h"
+#include "json/json.h"
+
+#define FRAMEINFOOFFSET 14
+#define MEMORY_ALLOT_ERROR -1
+#define CAM_RED_BALANCE_CHANNEL (1)
+#define CAM_GREEN_BALANCE_CHANNEL (2)
+#define CAM_BLUE_BALANCE_CHANNEL (3)
+
+#define SOFT_TRIGGER 21
+#define HARD_TRIGGER 20
+
+#define TRIGER_LINE0 0
+#define TRIGER_LINE1 1
+#define TRIGER_LINE2 2
+#define TRIGER_LINE3 3
+
+#define TRIGER_FALLING 0
+#define TRIGER_RISING  1
+
+class Camera
+{
+
+private:
+    GX_DEV_HANDLE g_device_;                                    ///< 设备句柄
+    GX_FRAME_DATA g_frame_data_;                                ///< 采集图像参数
+    void *g_raw8_buffer_;                                       ///< 将非8位raw数据转换成8位数据的时候的中转缓冲buffer
+    void *g_rgb_frame_data_;                                    ///< RAW数据转换成RGB数据后的存储空间，大小是相机输出数据大小的3倍
+    int64_t g_pixel_format_;                                    ///< 当前相机的pixelformat格式
+    int64_t g_color_filter_;                                    ///< bayer插值的参数
+    pthread_t g_acquire_thread_;                                ///< 采集线程ID
+    bool g_get_image_;                                          ///< 采集线程是否结束的标志：true 运行；false 退出
+    void *g_frameinfo_data_;                                    ///< 帧信息数据缓冲区
+    size_t g_frameinfo_datasize_;                               ///< 帧信息数据长度
+    pthread_mutex_t* mutex_;                                    ///< 线程互斥锁
+
+    int cam_id_;                                                ///< 相机编号
+
+    CTimeCounter g_time_counter_;                               ///< 计时器
+
+    std::vector<ROI> ROIs_;                                     ///< ROI
+    std::queue<std::string>* unsolved_list_;                    ///< 未处理图像文件名列表
+    std::deque<std::string>* work_name_list_;                   ///< 作业名列表
+    std::deque<int64_t>* work_count_list_;                      ///< 作业数列表
+    std::vector<std::vector<ROI>>* batch_ROI_list_;             ///< ROI列表
+    std::vector<std::vector<ROI>>::iterator batch_ROI_iter_;    ///< ROI列表迭代器
+
+    // param
+    double shutter_time_;
+    double red_balance_;
+    double green_balance_;
+    double blue_balance_;
+    double gain_value_;
+
+    int triger_line_;
+    int triger_edge_;
+
+    std::string file_dir_;
+    int64_t count_;
+
+    int fake_ptr_;
+
+protected:
+
+    //释放资源
+    int UnPreForImage();
+
+    //获取当前帧号
+    int GetCurFrameIndex();
+
+    // 线程入口
+    static void* ProcGetImage_pth(void* arg);
+    
+    //采集线程函数
+    void ProcGetImage();
+
+    //保存数据到PPM文件
+    void SavePPMFile(void *image_buffer, size_t width, size_t height);
+
+    // 保存黑白图
+    void SaveMono(void *image_buffer, size_t width, size_t height);
+
+    // 保存ROI中的图像到PPM
+    void SavePPMwithROIs(void *image_buffer, size_t width, size_t height, std::vector<ROI> ROIs);
+
+public:
+    Camera();
+    Camera( pthread_mutex_t* mutex, 
+            std::queue<std::string>* unsolved_list);
+    Camera( pthread_mutex_t* mutex, 
+            std::queue<std::string>* unsolved_list, 
+            std::deque<int64_t>* work_count_list, 
+            std::vector<std::vector<ROI>>* work_ROI_list,
+            int64_t pixel_format = GX_PIXEL_FORMAT_BAYER_GR8);
+    Camera( pthread_mutex_t* mutex,
+            std::queue<std::string>* unsolved_list, 
+            std::deque<std::string>* work_name_list, 
+            std::deque<int64_t>* work_count_list,
+            int64_t pixel_format = GX_PIXEL_FORMAT_BAYER_GR8);
+    ~Camera();
+    
+    // 相机初始化
+    int init();
+    // 获取图像大小并申请图像数据空间
+    int PreForImage();
+    // 开始采集
+    int start();
+    // 停止相机
+    int stop();
+    // 设置触发
+    int setTrigger(int type = SOFT_TRIGGER);
+    // 设置曝光
+    int setShutter(double shutter_time);
+    // 设置白平衡
+    int setBalance(double ratio, int channel);
+    // 设置增益
+    int setGain(double gain_value);
+    // 进行软触发
+    int sendSoftTrigger();
+    // 设置ROI
+    void setROI(std::vector<ROI> rois);
+    // 读取并设置参数
+    int applyParam();
+    // 读取相机拍摄计数
+    int64_t getCount();
+    // 修正fake_ptr位置
+    int popList();
+};
+
+
+
+#endif // CAMERA_H
---
+++ include/Detector.h
@@ -0,0 +1,157 @@
+#if !defined(DETECTOR_H)
+#define DETECTOR_H
+
+#include "DetectUtils.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <queue>
+#include "utils.h"
+#include "Serial.h"
+#include "Camera.h"
+#include "json/json.h"
+
+// enum detectType{
+//     DETECTA4 = 1,
+//     DETECTEACH = 0xfffe,
+//     SECDETECT = 2,
+//     FIRSTDETECT = 0xfffd
+// };
+
+// #define DETECTEACH 0
+#define DETECTA4 1
+#define SECDETECT 2
+
+class Detector
+{
+private:
+    // 图
+    cv::Mat template_img_;
+    cv::Mat template_label_;
+    cv::Mat template_img_resize_;
+    cv::Mat img_gray_;
+    cv::Mat img_resize_;
+    cv::Mat label_;
+    std::vector<Point2f> img_points_;
+    
+    // 线程
+    pthread_mutex_t* mutex_;
+    pthread_mutex_t* result_mutex_;
+    pthread_t detection_thread_; 
+    std::queue<string>* unsolved_list_;
+
+    // 参数
+    float k_pos_;
+    float k_scratch_;
+    float k_bigpro_;
+    string template_dir_;
+    string img_dir_;
+
+    // 计数器
+    int64_t count_;
+    int64_t id_;
+
+    // 类型 0-检测A4 1-检测单张
+    int8_t input_type_;
+
+    // 期望尺寸
+    float d_width_;
+    float d_height_;
+
+    // log
+    ofstream result_log_;
+
+    // 阈值
+    uint8_t bin_thresh_;
+    int bigpro_thresh_;
+    int scratch_thresh_;
+    int pos_thresh_;
+    float size_thresh_;
+
+    // 开关
+    bool save_img_switch_;
+    bool save_result_switch_;
+    bool show_time_switch_;
+    bool log_switch_;
+    bool start_detect_;
+
+    // 原图是否已设置标志位
+    bool origin_flag;
+
+    // 批次信息
+    std::deque<std::string>* work_name_list_;
+    std::deque<int64_t>* work_count_list_;
+    std::deque<string>* batch_origin_list_;
+    std::deque<int64_t>* batch_count_list_;
+    std::vector<cv::Point2i>* desired_size_list_;
+    std::vector<cv::Point2i>::iterator desired_size_iter_;
+
+    // 返回信息
+    Json::Value* result_root_;
+
+    // 相机指针
+    Camera* camera_;
+
+    // 串口指针
+    Serial* serial_;
+
+protected:
+    void findLabel(cv::Mat image_gray, cv::Mat &match_templ, std::vector<cv::Point2f> & points);
+    cv::Mat getLabelImg(Mat img);
+    void getROI(cv::Mat image, cv::Mat &dst);
+    cv::Mat LOG(Mat img);
+    void highConstract(Mat img, Mat & dst, int r);
+    cv::Mat search(Mat img, Mat templ);
+    void saveImg(string pre, Mat img);
+    int checkSize();
+    int checkPos();
+    int checkScratch();
+    int checkBigProblem();
+    void ProcDetect();
+    static void* detector_pth(void* args);
+    void writeResJson(int8_t result);
+    // void crossIntegral(cv::Mat M1, cv::Mat M2, cv::Mat& crosssum);
+    // cv::Mat getMerge(Mat src,int m,int n,int width,int height,int style);
+    // cv::Mat getSum(Mat src,int m,int n,int width,int height);
+    // cv::Mat getNCC(Mat origin_img,Mat img,Mat ointegral, Mat iintegral, int m, int n,int width,int height);
+
+public:
+    Detector();
+    Detector(pthread_mutex_t* mutex, std::queue<string>* list);
+    Detector(pthread_mutex_t* mutex, 
+            std::queue<string>* list, 
+            std::deque<string>* batch_origin_list, 
+            std::deque<int64_t>* batch_count_list, 
+            std::vector<cv::Point2i>* desired_size_list, 
+            pthread_t threadId = 2);
+
+    Detector(pthread_mutex_t* mutex, 
+            pthread_mutex_t* result_mutex, 
+            Json::Value* root, 
+            std::queue<string>* list, 
+            std::deque<std::string>* work_name_list_, 
+            std::deque<int64_t>* work_count_list_, 
+            std::deque<string>* batch_origin_list, 
+            std::deque<int64_t>* batch_count_list, 
+            int detectionId = 0, 
+            pthread_t threadId = 2);
+    ~Detector();
+    int setParam();
+    int detect();
+    int launchThread();
+    int setOriginImg(string filename);
+    int setOriginImg(Mat img);
+    void setDesiredSize(cv::Point2i desired_size);
+    void setImg(string filename);
+    void setImg(Mat img);
+    void setCameraPtr(Camera* camera);
+    void setSerialPtr(Serial* serial);
+    void setThresh();
+    int stopThread();
+    int sendMsg();
+    int64_t getCount();
+};
+
+
+#endif // DETECTOR_H
